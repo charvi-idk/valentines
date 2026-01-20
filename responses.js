60a5612272d45dd4810ab3a036a1c81c5e19b751
Patel Charvi (2024) ZNAC425 <charvi.patel.2024@live.rhul.ac.uk>
2026-01-20 18:53:15 +0000

    added files

---
+++ responses.js
@@ -0,0 +1,11 @@
+
+const noTexts = [
+  "No 😢",
+  "Are you sure?",
+  "I'm going to cry",
+  "That hurts my feelings",
+  "Please reconsider",
+  "I'm literally sobbing",
+  "Okay wow",
+  "This is devastating"
+];
